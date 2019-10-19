6f8ede8dfd597eca6bea672e43a1bbe6969a91bd
Kaarthun <47163285+Kaarthun@users.noreply.github.com>
2019-10-19 11:06:18 -0500

    Added Activity for Shopping List

---
+++ app/src/main/java/com/core/android/pocketbar/ShoppingList.java
@@ -0,0 +1,14 @@
+package com.core.android.pocketbar;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShoppingList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_list);
+    }
+}
