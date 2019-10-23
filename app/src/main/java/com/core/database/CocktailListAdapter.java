50e11e07f31d17212512e7b5ebaadf97aff3fba4
tnovakgh <novak_troy@yahoo.com>
2019-10-23 15:33:36 -0500

    Added RecipeActivity and got it working to at least display some of the data. Just need to refine it and figure out how to get the rest of the cocktail's data.

--- app/src/main/java/com/core/database/CocktailListAdapter.java
+++ app/src/main/java/com/core/database/CocktailListAdapter.java
@@ -1,6 +1,7 @@
 package com.core.database;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,16 +10,24 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.core.android.pocketbar.R;
+import com.core.android.pocketbar.RecipeActivity;
 
 import java.util.List;
 
 public class CocktailListAdapter extends RecyclerView.Adapter<CocktailListAdapter.CocktailViewHolder> {
 
+    // key for intent extra
+    public static final String NAME_MESSAGE = "com.core.android.pocketbar.NAME";
+    public static final String DIRECTIONS_MESSAGE = "com.core.android.pocketbar.DIRECTIONS";
     //Builds a cocktail view from a xml specification
     private final LayoutInflater mInflater;
     private List<Cocktail> mCocktails; //Cocktails the adapter is managing
+    private Context context;    // used to start RecipeActivity
 
-    public CocktailListAdapter(Context context) { mInflater = LayoutInflater.from(context); }
+    public CocktailListAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        this.context = context;
+    }
 
     /**
      *
@@ -52,12 +61,27 @@ public class CocktailListAdapter extends RecyclerView.Adapter<CocktailListAdapte
         else return 0;
     }
 
-    class CocktailViewHolder extends RecyclerView.ViewHolder {
+    class CocktailViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private final TextView cocktailItemView;
 
         private CocktailViewHolder(View itemView) {
             super(itemView);
             cocktailItemView = itemView.findViewById(R.id.textView);
+            itemView.setOnClickListener(this);
+        }
+
+        public void onClick(View v){
+            Intent intent = new Intent(context, RecipeActivity.class);
+            // get position of item that was clicked
+            int mPosition = getLayoutPosition();
+            // use ^^ that to access the affected item in mWordList (LinkedList holding data)
+            String cocktailName = mCocktails.get(mPosition).getName();
+            String mixingDirections = mCocktails.get(mPosition).getDirections();
+            // add text to the 'Intent' as an extra
+            intent.putExtra(NAME_MESSAGE, cocktailName);
+            intent.putExtra(DIRECTIONS_MESSAGE, mixingDirections);
+
+            context.startActivity(intent);
         }
     }
 }
