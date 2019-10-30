c6febcf531539fda6af0418fcdb36120d92410c9
Arick Aarthun <aricka@rticorp.com>
2019-10-29 21:38:46 -0500

    button click prints message to log

--- app/src/main/java/com/core/android/pocketbar/ShoppingList.java
+++ app/src/main/java/com/core/android/pocketbar/ShoppingList.java
@@ -1,6 +1,9 @@
 package com.core.android.pocketbar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +14,21 @@ public class ShoppingList extends AppCompatActivity {
 
     private final LinkedList<String> shoppingList = new LinkedList<>();
 
+    private EditText mAddItemEntry;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_shopping_list);
+
+        mAddItemEntry = findViewById(R.id.editText_shoplistadd);
+
+    }
+
+    public void addItem(View view) {
+
+        String ingredientToAdd = mAddItemEntry.getText().toString();
+        Log.i("AddItem Method", "The Following Was Added: " + ingredientToAdd);
     }
 }
