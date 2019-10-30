5e58fc4a6ed4be38c43b6cb00b59bf97a47d149a
Kaarthun <47163285+Kaarthun@users.noreply.github.com>
2019-10-29 20:43:33 -0500

    minor changes

--- app/src/main/java/com/core/android/pocketbar/ShoppingList.java
+++ app/src/main/java/com/core/android/pocketbar/ShoppingList.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.LinkedList;
+
 public class ShoppingList extends AppCompatActivity {
+
+
+    private final LinkedList<String> shoppingList = new LinkedList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
+        setContentView(R.layout.content_shopping_list);
     }
 }
