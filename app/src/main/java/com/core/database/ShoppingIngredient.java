b4d6ba910c25c3042f18669caf69afe4a77a0e26
Arick Aarthun <aricka@rticorp.com>
2019-11-02 13:04:35 -0500

    updating for convo with Drew

---
+++ app/src/main/java/com/core/database/ShoppingIngredient.java
@@ -0,0 +1,25 @@
+package com.core.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+
+@Entity(tableName = "shopping_ingredient", primaryKeys = {"ingredient"})
+public class ShoppingIngredient {
+
+
+    @NonNull
+    @ColumnInfo(name = "ingredient")
+    private String ingredient;
+
+
+    public ShoppingIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    @NonNull
+    public String getIngredient() {
+        return ingredient;
+    }
+
+}
